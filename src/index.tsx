31b197d749238954167b217d65ed428c5acabc41
Lars Mennen <mennen.lars@gmail.com>
2020-03-29 02:05:11 +0100

    Add Google Analytics

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import App from "./App";
 import * as serviceWorker from './serviceWorker';
 import * as Sentry from '@sentry/browser';
+import ReactGA from 'react-ga';
 
 import './styles.scss';
 
 Sentry.init({dsn: "https://5f0ebb4296f04182a271364dc69c5b9c@sentry.io/5178703"});
+
+ReactGA.initialize('UA-162057967-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
     <React.StrictMode>
